3ccaa65e888e8fa3458af8fe0fec7e72960e0d53
Steve Putman <steve.putman@gmail.com>
2021-09-01 21:53:48 -0500

    Work on paged queries

--- R/error_handling.R
+++ R/error_handling.R
@@ -1,38 +1,40 @@
 #' Check response for errors
 #' @description
-#' If the API returns 400, there was a problem with the provided parameters. Raise DAQueryException.
-#' If the API returns 400 and request was to /tokens endpoint, likely bad credentials. Raise DAAuthException.
+#' If the API returns 400, there was a problem with the provided parameters; stop
+#' If the API returns 400 and request was to /tokens endpoint, likely bad credentials; stop.
 #' If the API returns 401, refresh access token if found and resend request.
 #' If the API returns 403 and request was to /tokens endpoint, sleep for 60 seconds and try again.
-#' If the API returns 404, an invalid dataset name was provided. Raise DADatasetException.
-#' 5xx errors are handled by the session's Retry configuration. Debug logging returns retries remaining.
-#' @param response [xx-formatted] HTTP response
-#' @return Any error messages
-check_response <- function(response, querydataset, ...) {
+#' If the API returns 404, an invalid dataset name was provided; stop.
+#' 5xx errors are re-sent through a normal `RETRY`
+#' @param response HTTP response
+#' @return string to determine retry strategy
+check_response <- function(response) {
   tokenrequest <- stringr::str_detect(response$url, "tokens")
   if (httr::http_error(response)) {
     if (httr::status_code(response) == 400) {
       if (tokenrequest) {
         httr::stop_for_status(response, "get token")
       } else {
-        httr::stop_for_status(response, "complete query")
+        httr::stop_for_status(response, "complete query. There was a problem with the supplied parameters")
       }
     }
     if (httr::status_code(response) == 401) {
       Sys.setenv("ENVERUS_ACCESS_TOKEN" = "")
       get_access_token()
       httr::message_for_status(response, "authenticate; token missing or expired. Obtaining new token")
-      query(querydataset, ...)
+      return("manretry")
     }
     if (httr::status_code(response) == 403 & tokenrequest) {
       httr::message_for_status(response, "obtain token. Waiting 60 seconds to retry")
       Sys.sleep(60)
-      query(querydataset, ...)
+      return("manretry")
     }
     if (httr::status_code(response) == 404) {
-      httr::warn_for_status(response, "locate dataset.")
+      httr::stop_for_status(response, "locate dataset.")
     }
+    httr::warn_for_status(response, "connect to server. Retrying")
+    return("autoretry")
   } else {
-    return(response)
+    return("ok")
   }
 }
--- R/query.R
+++ R/query.R
@@ -1,19 +1,63 @@
 #' @export
-query <- function(dataset, ...) {
+query <- function(dataset, manretry = 2, ...) {
   access_token <- Sys.getenv("ENVERUS_ACCESS_TOKEN")
   api_key <- Sys.getenv("ENVERUS_API_KEY")
   options = list(...)
   apiurl <- "https://di-api.drillinginfo.com/v2/direct-access"
   queryurl <- glue::glue("{apiurl}/{dataset}")
-  queryresult <- httr::GET(queryurl,
+  queryresponse <- httr::GET(queryurl,
                              encode = "json",
                              httr::add_headers("X-API-KEY" = api_key,
                                                "Authorization" = glue::glue("Bearer {access_token}")),
                              httr::verbose())
-  check_response(queryresult, dataset, ...)
+  errors <- check_response(queryresponse)
+  if (errors == "manretry") {
+    if (manretry > 0) {
+      manretry <- manretry - 1
+      query(dataset, manretry, ...)
+    } else {
+      stop("Still failing; stopping.")
+    }
+  }
+  if (errors == "autoretry") {
+    queryresponse <- httr::RETRY("GET",
+                                 queryurl,
+                                 encode = "json",
+                                 httr::add_headers("X-API-KEY" = api_key,
+                                                   "Authorization" = glue::glue("Bearer {access_token")),
+                                 httr::verbose())
+  }
+  if (errors == "ok") {
+    content <- httr::content(queryresponse)
+    while (stringr::str_detect(queryresponse$headers$links, "next")) {
+      nexturl <- stringr::str_match(queryresponse$headers$links, "<(.*?)>")[[2]]
+      queryurl <- glue::glue(apiurl, nexturl)
+      queryresponse <-   httr::GET(queryurl,
+                                   encode = "json",
+                                   httr::add_headers("X-API-KEY" = api_key,
+                                   "Authorization" = glue::glue("Bearer {access_token}")),
+                                    httr::verbose())
+      content <- c(content, httr::content(queryresponse))
+    }
+    return(content)
+  }
 }
+
 
 #' @export
 enverus2 <- function(dataset, ...) {
-  query(dataset, ...)
+  initresp <- query(dataset, ...)
 }
+
+#' @export
+testget <- function(statuscode) {
+  resp <- httr::GET(glue::glue("https://httpbin.org/status/{statuscode}"))
+  check_response(resp)
+  message("am i still going?")
+}
+
+      queryresponse <-   httr::HEAD(queryurl,
+                                   encode = "json",
+                                   httr::add_headers("X-API-KEY" = api_key,
+                                   "Authorization" = glue::glue("Bearer {access_token}")),
+                                    httr::verbose())
